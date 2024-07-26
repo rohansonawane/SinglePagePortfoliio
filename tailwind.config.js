96e77660f581b6542163910bdb1d691566c544bb
Rohan Sonawane <rohansonawane28@gmail.com>
2024-07-25 23:33:57 -0700

    First Commit of Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,14 +15,21 @@ module.exports = {
     },
     screens: {
       "sm": "640px",
-      "sm": "768px",
-      "sm": "960px",
-      "sm": "1200px",
+      "md": "768px",
+      "lg": "960px",
+      "xl": "1200px",
     },
     fontFamily: {
       primary: "var(--font-jetbrainsMono)",
     },
     extend: {
+      colors: {
+        primary: '#1c1c22',
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        }
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
